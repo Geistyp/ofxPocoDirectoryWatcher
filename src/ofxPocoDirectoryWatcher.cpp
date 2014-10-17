cc947163e21117113afcf1ec562c9b2c0e1e5d9d
Geistyp <geistyp@gmail.com>
2014-10-17 17:38:08 +0800

    first add

---
+++ src/ofxPocoDirectoryWatcher.cpp
@@ -0,0 +1,46 @@
+#include "ofxPocoDirectoryWatcher.h"
+
+
+ofxPocoDirectoryWatcher::ofxPocoDirectoryWatcher()
+{
+}
+
+
+ofxPocoDirectoryWatcher::~ofxPocoDirectoryWatcher()
+{
+}
+
+void ofxPocoDirectoryWatcher::watch(string path, int eventMask /*= Poco::DirectoryWatcher::DW_FILTER_ENABLE_ALL*/, int scanInterval /*= Poco::DirectoryWatcher::DW_DEFAULT_SCAN_INTERVAL*/)
+{
+	watcher = new Poco::DirectoryWatcher(path, eventMask, scanInterval);
+	watcher->itemAdded += Poco::delegate(this, &ofxPocoDirectoryWatcher::onItemAdded);
+	watcher->itemModified += Poco::delegate(this, &ofxPocoDirectoryWatcher::onItemModified);
+	watcher->itemMovedFrom += Poco::delegate(this, &ofxPocoDirectoryWatcher::onItemMovedFrom);
+	watcher->itemMovedTo += Poco::delegate(this, &ofxPocoDirectoryWatcher::onItemMovedTo);
+	watcher->itemRemoved += Poco::delegate(this, &ofxPocoDirectoryWatcher::onItemRemoved);
+}
+
+void ofxPocoDirectoryWatcher::onItemAdded(const Poco::DirectoryWatcher::DirectoryEvent& event)
+{
+	ofSendMessage("Added:" + event.item.path());
+}
+
+void ofxPocoDirectoryWatcher::onItemModified(const Poco::DirectoryWatcher::DirectoryEvent& event)
+{
+	ofSendMessage("Modified:" + event.item.path());
+}
+
+void ofxPocoDirectoryWatcher::onItemMovedFrom(const Poco::DirectoryWatcher::DirectoryEvent& event)
+{
+	ofSendMessage("MovedFrom:" + event.item.path());
+}
+
+void ofxPocoDirectoryWatcher::onItemMovedTo(const Poco::DirectoryWatcher::DirectoryEvent& event)
+{
+	ofSendMessage("MovedTo:" + event.item.path());
+}
+
+void ofxPocoDirectoryWatcher::onItemRemoved(const Poco::DirectoryWatcher::DirectoryEvent& event)
+{
+	ofSendMessage("Removed:" + event.item.path());
+}
---
+++ src/ofxPocoDirectoryWatcher.h
@@ -0,0 +1,43 @@
+//  [10/17/2014 GeistYp]
+#pragma once
+
+#include "ofMain.h"
+
+#include "Poco/Delegate.h"
+#include "Poco/DirectoryWatcher.h"
+
+/*
+ *	Use Poco::DirectoryWatcher
+ *  see: http://pocoproject.org/docs/Poco.DirectoryWatcher.html
+ */
+class ofxPocoDirectoryWatcher
+{
+public:
+	ofxPocoDirectoryWatcher();
+	~ofxPocoDirectoryWatcher();
+
+	/*
+	DirectoryEventMask
+		DW_FILTER_ENABLE_ALL = 31
+	Enables all event types.
+		DW_FILTER_DISABLE_ALL = 0
+	Disables all event types.
+		DirectoryEventType
+
+	DW_ITEM_ADDED = 1
+	DW_ITEM_REMOVED = 2
+	DW_ITEM_MODIFIED = 4
+	DW_ITEM_MOVED_FROM = 8
+	DW_ITEM_MOVED_TO = 16
+	*/
+	void watch(string path, int eventMask = Poco::DirectoryWatcher::DW_FILTER_ENABLE_ALL, int scanInterval = Poco::DirectoryWatcher::DW_DEFAULT_SCAN_INTERVAL);
+
+	void onItemAdded(const Poco::DirectoryWatcher::DirectoryEvent& event);			// Fired when a file or directory has been created or added to the directory.
+	void onItemModified(const Poco::DirectoryWatcher::DirectoryEvent& event);		// Fired when a file or directory has been modified.
+	void onItemMovedFrom(const Poco::DirectoryWatcher::DirectoryEvent& event);		// Fired when a file or directory has been renamed. This event delivers the old name.
+	void onItemMovedTo(const Poco::DirectoryWatcher::DirectoryEvent& event);		// Fired when a file or directory has been moved. This event delivers the new name.
+	void onItemRemoved(const Poco::DirectoryWatcher::DirectoryEvent& event);		// Fired when a file or directory has been removed from the directory.
+
+	Poco::DirectoryWatcher* watcher;
+};
+
